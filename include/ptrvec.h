4a5905e950d6823e6fafd1e8a76d846b7f0aac49
andre-1337 <beethovencw1991@gmail.com>
2022-03-17 19:35:22 +0000

    ✅ Add  rptrvec.h

---
+++ include/ptrvec.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct Ptrvec {
+    int32_t len;
+    int32_t cap;
+    void **elements;
+} Ptrvec;
+
+Ptrvec ptrvec_create();
+Ptrvec ptrvec_with_cap(int32_t cap);
+bool ptrvec_is_empty(Ptrvec *v);
+void *ptrvec_pop(Ptrvec *v);
+void ptrvec_append(Ptrvec *v, Ptrvec *other);
+void ptrvec_push_ptr(Ptrvec *v, void *elem);
+void *ptrvec_get(Ptrvec *v, int32_t i);
+void ptrvec_set(Ptrvec *v, int32_t, void *elem);
+void ptrvec_resize_s(Ptrvec *v, int32_t size);
+void ptrvec_resize(Ptrvec *v);
+void ptrvec_free(Ptrvec *v);
