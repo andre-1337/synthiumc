d9352aaab336dd3ab9a7a4526a4bbcb6b38b79d4
andre-1337 <beethovencw1991@gmail.com>
2022-03-17 23:26:08 +0000

    ✅ Update mod.h

--- include/mod.h
+++ include/mod.h
@@ -7,3 +7,45 @@
 
 #include "ty.h"
 #include "ast.h"
+
+#define SYNTHIUM_FILE_ENDING ".syn"
+
+typedef struct Module {
+    Ptrvec statements;
+    Path path;
+    Vec imports;
+    Vec functions;
+    Vec structs;
+    Mod *ty;
+    int32_t idx;
+} Module;
+
+typedef struct ModuleMap {
+    Ptrvec mods;
+    Map mod_paths;
+} ModuleMap;
+
+Module mod_create(Path p);
+Ty *mod_s_lookup(Module *m, int32_t key_len, char *const key);
+Stmt *mod_get_stmt_at(Module *m, int32_t i);
+void mod_set_stmt_at(Module *m, int32_t i, Stmt *s);
+int32_t mod_num_stmts(Module *m);
+Stmt *mod_get_special_stmt_at(Module *m, Vec *indices, int32_t i);
+ImportStmt *mod_get_import_at(Module *m, int32_t i);
+int32_t mod_num_imports(Module *m);
+FuncDeclStmt *mod_get_function_at(Module *m, int32_t i);
+int32_t mod_num_functions(Module *m);
+StructDeclStmt *mod_get_struct_at(Module *m, int32_t i);
+int32_t mod_num_structs(Module *m);
+void mod_push_stmt(Module *m, Stmt *stmt);
+Stmt *mod_get_stmt(Module *m, int32_t i);
+void mod_free_m(Module *m);
+ModuleMap mod_map_with_cap(int32_t size);
+int32_t mod_num_mods(ModuleMap *mm);
+void mod_add_mod(Map *mm, Module *m);
+Module *mod_get_mod(ModuleMap *mm, int32_t i);
+int32_t mod_get_mod_idx(ModuleMap *mm, Path *abs_path);
+Module *mod_get_mod_by_path(ModuleMap *mm, Path *abs_path);
+void mod_map_free(ModuleMap *mm);
+int32_t mod_get_abs_import_path(Module *m, ImportStmt *imp, SpanInterner *si, PathBuf *dest);
+Module *mod_try_get_mod_from_import(ModuleMap *mm, Module *m, SpanInterner *si, ImportStmt *imp, char *const errdest);
