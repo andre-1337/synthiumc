899f9a70775015c21f8ae7d48de29be5550df096
andre-1337 <beethovencw1991@gmail.com>
2022-03-17 19:44:25 +0000

    ✅ Add precedence.h

---
+++ include/precedence.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stdint.h>
+
+#include "lexer.h"
+
+typedef enum Precedence : int32_t {
+    PRECEDENCE_NONE,
+    PRECEDENCE_ASSIGN,
+    PRECEDENCE_AND,
+    PRECEDENCE_EQUALITY,
+    PRECEDENCE_COMPARISON,
+    PRECEDENCE_SUM,
+    PRECEDENCE_PRODUCT,
+    PRECEDENCE_UNARY,
+    PRECEDENCE_CALL,
+    PRECEDENCE_ACCESS
+} Precedence;
+
+int32_t precedence_get(TokenType ty);
