78f0cb039a13be03148c69044ebea92369eca939
andre-1337 <beethovencw1991@gmail.com>
2022-03-18 17:01:54 +0000

    ✅ Add tuple.h header

---
+++ include/tuple.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+typedef struct Tuple {
+    int32_t orig_idx;
+    int32_t value;
+} Tuple;
+
+void tuple_swap(Tuple *first, Tuple *second);
+void tuple_inner_sort(Tuple *arr, int32_t len);
+int32_t *tuple_isort(const int32_t *import_nums, int32_t len);
