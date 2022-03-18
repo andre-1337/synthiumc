26f1b9085341ebfa0b48affabdf4a9609416d7cd
andre-1337 <beethovencw1991@gmail.com>
2022-03-18 00:52:30 +0000

    ✅ Update ast.h

--- include/ast.h
+++ include/ast.h
@@ -66,12 +66,18 @@ typedef enum StmtType {
     STMT_RETURN
 } StmtType;
 
+typedef enum ElseType {
+    ELSE_TYPE_NO_ELSE,
+    ELSE_TYPE_BLOCK,
+    ELSE_TYPE_ELSE_IF
+} ElseType;
+
 typedef struct Stmt {
     int32_t tag;
 } Stmt;
 
 typedef struct Expr {
-    int32_t tag;
+    ExprType tag;
     Ty *ty;
     Span span;
 } Expr;
@@ -213,3 +219,115 @@ typedef struct UnaryExpr {
     UnaryType ty;
 } UnaryExpr;
 
+Stmt *ast_new_expr_stmt(Expr *e);
+bool ast_is_expr_stmt(Stmt *s);
+ExprStmt *ast_as_expr_stmt(Stmt *s);
+
+Stmt *ast_new_delete_stmt(Expr *e);
+bool ast_is_delete_stmt(Stmt *s);
+DeleteStmt *ast_as_delete_stmt(Stmt *s);
+
+Stmt *ast_new_return_stmt(Expr *e);
+bool ast_is_return_stmt(Stmt *s);
+ReturnStmt *ast_as_return_stmt(Stmt *s);
+
+Stmt *ast_new_while_stmt(Expr *condition, BlockStmt *block);
+bool ast_is_while_stmt(Stmt *s);
+WhileStmt *ast_as_while_stmt(Stmt *s);
+
+Stmt *ast_new_if_stmt(Expr *condition, BlockStmt *block, Stmt *else_stmt);
+bool ast_is_if_stmt(Stmt *s);
+IfStmt *ast_as_if_stmt(Stmt *s);
+ElseType ast_else_type(IfStmt *s);
+
+Stmt *ast_new_block_stmt(Ptrvec statements);
+bool ast_is_block_stmt(Stmt *s);
+BlockStmt *ast_as_block_stmt(Stmt *s);
+
+Stmt *ast_new_func_decl_stmt(Token ident, ParamList params, Type ret_ty, bool is_extern, BlockStmt *block);
+bool ast_is_func_decl_stmt(Stmt *s);
+FuncDeclStmt *ast_as_func_decl_stmt(Stmt *s);
+
+Stmt *ast_new_struct_decl_stmt(SpanInterner *si, Ident name, Vec fields);
+bool ast_is_struct_decl_stmt(Stmt *s);
+StructDeclStmt *ast_as_struct_decl_stmt(Stmt *s);
+
+Stmt *ast_new_import_stmt(Span span, char *const path);
+bool ast_is_import_stmt(Stmt *s);
+ImportStmt *ast_as_import_stmt(Stmt *s);
+
+Stmt *ast_new_let_stmt(Token ident, Type ty, Expr *value);
+bool ast_is_let_stmt(Stmt *s);
+LetStmt *ast_as_let_stmt(Stmt *s);
+
+bool ast_has_type_decl(LetStmt *s);
+void ast_stmt_free(Stmt *s);
+
+Expr *ast_new_access_expr(Span span, Expr *left, Expr *right);
+bool ast_is_access_expr(Expr *e);
+AccessExpr *ast_as_access_expr(Expr *e);
+
+Expr *ast_new_as_expr(Span span, Expr *e, Type ty);
+bool ast_is_as_expr(Expr *e);
+AsExpr *ast_as_as_expr(Expr *e);
+
+Expr *ast_new_new_expr(Span span, Expr *e);
+bool ast_is_new_expr(Expr *e);
+NewExpr *ast_as_new_expr(Expr *e);
+
+Expr *ast_new_call_expr(Span span, Expr *ident, ArgList args);
+bool ast_is_call_expr(Expr *e);
+CallExpr *ast_as_call_expr(Expr *e);
+
+Expr *ast_new_init_expr(Span span, Expr *ident, InitList inits);
+bool ast_is_init_expr(Expr *e);
+InitExpr *ast_as_init_expr(Expr *e);
+
+Expr *ast_new_assign_expr(Span span, Expr *left, Expr *right);
+bool ast_is_assign_expr(Expr *e);
+AssignExpr *ast_as_assign_expr(Expr *e);
+
+Expr *ast_new_ident_expr(Token ident);
+bool ast_is_ident_expr(Expr *e);
+IdentExpr *ast_as_ident_expr(Expr *e);
+
+Expr *ast_new_int_expr(Span span, char *const ptr);
+bool ast_is_int_expr(Expr *e);
+IntExpr *ast_as_int_expr(Expr *e);
+
+Expr *ast_new_string_expr(Span span, char *const ptr);
+bool ast_is_string_expr(Expr *e);
+StringExpr *ast_as_string_expr(Expr *e);
+
+Expr *ast_new_char_expr(Span span, char *const ptr);
+bool ast_is_char_expr(Expr *e);
+CharExpr *ast_as_char_expr(Expr *e);
+
+Expr *ast_new_binary_expr(Span span, BinaryType ty, Expr *left, Expr *right);
+bool ast_is_binary_expr(Expr *e);
+BinaryExpr *ast_as_binary_expr(Expr *e);
+
+Expr *ast_new_unary_expr(Span span, UnaryType kind, Expr *right);
+bool ast_is_unary_expr(Expr *e);
+UnaryExpr *ast_as_unary_expr(Expr *e);
+
+Expr ast_create_expr(ExprType tag, Span span);
+void ast_expr_free(Expr *e);
+char *const ast_expr_to_string(Expr *e, SpanInterner *si);
+
+ArgList ast_create_arg_list();
+int32_t ast_num_args(ArgList *al);
+Expr *ast_get_arg_al(ArgList *al, int32_t i);
+void ast_push_arg(ArgList *al, Expr *arg);
+void ast_free_al(ArgList *al);
+char *const ast_arg_list_to_string(ArgList *al, SpanInterner *si);
+
+Init ast_create_init(Ident ident, Expr *expr);
+Init ast_empty_init();
+InitList ast_create_init_list();
+int32_t ast_num_inits(InitList *il);
+Expr *ast_get_init_expr(InitList *il, int32_t i);
+bool ast_get_init_at(InitList *il, int32_t i, Init *dest);
+void ast_push_init(InitList *il, Init init);
+void ast_free_il(InitList *il);
+char *const ast_init_list_to_string(InitList *il, SpanInterner *si);
