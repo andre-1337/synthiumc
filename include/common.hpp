552a658e3deb4a731e8a6ac1cf2ceafa1240f74d
andre-1337 <beethovencw1991@gmail.com>
2022-04-28 18:40:45 +0100

    indev commit

---
+++ include/common.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+
+using u8 = unsigned char;
+using u16 = unsigned short;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+
+using i8 = signed char;
+using i16 = signed short;
+using i32 = signed int;
+using i64 = signed long long;
+
+using f32 = float;
+using f64 = double;
+using f80 = long double;
+
+using string = std::string &;
